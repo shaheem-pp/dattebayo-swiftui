ea6ac2f42e3f23e946728699885b36f1b0f875a3
shaheem-pp <shanofficial2000@gmail.com>
2025-03-07 14:23:40 -0500

    Initial Commit

---
+++ dattebayo/dattebayoApp.swift
@@ -0,0 +1,17 @@
+//
+//  dattebayoApp.swift
+//  dattebayo
+//
+//  Created by Shaheem PP on 2025-03-07.
+//
+
+import SwiftUI
+
+@main
+struct dattebayoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ dattebayoTests/dattebayoTests.swift
@@ -0,0 +1,17 @@
+//
+//  dattebayoTests.swift
+//  dattebayoTests
+//
+//  Created by Shaheem PP on 2025-03-07.
+//
+
+import Testing
+@testable import dattebayo
+
+struct dattebayoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
